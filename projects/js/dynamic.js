65b1c4e89eb7053033a2374593cee3172e84d6c8
gshabeer <91020203+gshabeer@users.noreply.github.com>
2022-06-29 15:04:08 +0500

    second

---
+++ projects/js/dynamic.js
@@ -0,0 +1,267 @@
+let projects = [
+  {
+    sub_heading: "GS Keeps Website",
+    text: "This is note keeping website. Every note you save will remain until you yourself will delete them.",
+    img: "img/gs-keeps.webp",
+    ancher: "project/gsKeeps/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Todo WebApp",
+    text: "This is a Todo webapp. You can add as many todos as you want. You can also check them after completing and you can also view your completed todos by click completed button. They will be stored it means if you close your browser they will remain there. You can also edit them, delete them or remove them all.",
+    img: "img/todo.webp",
+    ancher: "project/todo/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Media Player WebApp",
+    text: "This is a media player web app with all basic functionalities like seekbar,next/previous,play/pause and volume control etc. Here you can also choose from playlist to play any song or whatever you want by clicking on I button. Also can be fullscreen.",
+    img: "img/player.webp",
+    ancher: "project/player/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Emoji Maker WebApp",
+    text: "This is an emoji making web app. Here you can create different type of emoji by choosing different different color, eyes and mouth combination.",
+    img: "img/emoji.webp",
+    ancher: "project/emoji/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Calculator Web App",
+    text: "This is a Calculator with all basic functionalties.",
+    img: "img/calculator.webp",
+    ancher: "project/calculator/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Calculator WebApp",
+    text: "This is a Calculator WebApp with scientific also. by clicking arrow button you can pull out scientific calculator.",
+    img: "img/calculator2.webp",
+    ancher: "project/calculator2/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Analog Clock WebApp",
+    text: "This is a simple analog clock with digital time also. I have created it using html css and javascript without using any image or svg for dial lines and numbers.",
+    img: "img/clock.webp",
+    ancher: "project/clock/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Google Clone Website",
+    text: "This is a Google clone website. The original website is very complex and a search engine but I created this website using only HTML and CSS.",
+    img: "img/google.webp",
+    ancher: "project/google/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Facebook Login Page Clone",
+    text: "This is a facebook login page clone with simple html and tailwing css.",
+    img: "img/fbook.webp",
+    ancher: "project/facebook/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "myCar Website",
+    text: "This is a car selling, repairing, and a car insurance website. Almost all kinds of cars repaired here.",
+    img: "img/my-car.webp",
+    ancher: "project/myCar/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "CodeWithHarry Clone Website",
+    text: "Basically this is a clone website of codewithharry.com. This website is created using Bootstrap, flask and Django. But I recreated this website using simple HTML and CSS.",
+    img: "img/codewith.webp",
+    ancher: "project/harry/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Linear Gradient Generator Website",
+    text: "A simple one page linear gradient generator. You can choose how many colors you want to generate. Every time you refresh the website you will get random color. Also you can copy colors to emplement in your website.",
+    img: "img/gradient-generator.webp",
+    ancher: "project/linear/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Linear Gradient Generator Website",
+    text: "A simple one page linear gradient generator. You can generate custom degree of your own choice. You can choose colors you want to generate. Also you can copy colors to implement in your website.",
+    img: "img/gradient-generator2.webp",
+    ancher: "project/linear2/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "FAQ's WebPage",
+    text: "This is simple FAQ's(Frequently Asked Questions) answer webpage. By opening one faq previous faq will be closed.",
+    img: "img/faq.webp",
+    ancher: "project/faq/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Drag n Drop Website",
+    text: "A Drag n Drop Website where by droping an image you can preview it or either browse it to preview it.",
+    img: "img/dragDrop.webp",
+    ancher: "project/drop/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Glass Design Website",
+    text: "This is a glass design info card. When mouse hover over it. It start tilting.",
+    img: "img/glass.webp",
+    ancher: "project/glass/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+  {
+    sub_heading: "Reviews Webpage",
+    text: "This is simple reviews webpage of customers. You can click next and previous to view more.",
+    img: "img/reviews.webp",
+    ancher: "project/review/index.html",
+    parent_class: "contents",
+    content_class: "content",
+    img_class: "img",
+    circle_class: "curcle",
+  },
+  {
+    sub_heading: "Bomb Design",
+    text: "This is a simple bomb design with css. Click on it to see magic.",
+    img: "img/bomb.webp",
+    ancher: "project/bomb/index.html",
+    parent_class: "reverse",
+    content_class: "content2",
+    img_class: "img3",
+    circle_class: "curcle2",
+  },
+];
+
+// Add Projects to body
+function addProjects() {
+  projects.map((currProject) => {
+    let contents = document.createElement("div");
+    contents.classList.add("contents", currProject.parent_class);
+    let project = `
+      <div class="${currProject.content_class}">
+      <h2 class="sub-heading">${currProject.sub_heading}</h2>
+      <p class="txt-content">${currProject.text}
+      <div class="btn p_btn" data-src="${currProject.img}" data-ancher="${currProject.ancher}"><button>See Project</button></div></p>
+      <div class="curcle ${currProject.circle_class}"></div></div>
+      <div class="img ${currProject.img_class}">
+      <img src="../load.webp" data-src="${currProject.img}" alt="Loading..." height="240" loading="lazy"></div>`;
+    contents.insertAdjacentHTML("afterbegin", project);
+    parentSec.appendChild(contents);
+  });
+}
+
+// Show images after load
+function imgReplace() {
+  const imgRef = document.querySelectorAll("img[data-src]");
+  let imgPreviewer = document.querySelector(".imgprev");
+
+  const imgObserver = new IntersectionObserver(
+    (entries) => {
+      const [entry] = entries;
+      let elm = entry.target;
+      if (entry.isIntersecting) {
+        elm.src = elm.dataset.src;
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+    }
+  );
+
+  imgRef.forEach((currImg) => {
+    imgObserver.observe(currImg);
+
+    imgPreviewer.addEventListener("click", function () {
+      this.classList.add("hide");
+    });
+  });
+}
+
+let menuBar = document.querySelector("header nav ul");
+let menuBtn = document.querySelector(".menu-bar");
+let parentSec = document.querySelector(".sec6");
+let backToTop = document.querySelector(".backToTop");
+
+// Menu Sidebar Open/Close
+menuBtn.addEventListener("click", () => {
+  menuBar.classList.toggle("active");
+});
+
+// Close Sidebar on Page Scroll
+window.onscroll = () => {
+  menuBar.classList.remove("active");
+  if (window.pageYOffset > 1000) {
+    backToTop.classList.remove("hide");
+  } else {
+    backToTop.classList.add("hide");
+  }
+};
+
+window.onload = () => {
+  // When loading end add projects
+  setTimeout(() => {
+    addProjects();
+    imgReplace();
+
+    let btnz = document.querySelectorAll(".p_btn");
+    let imgPreviewer = document.querySelector(".imgprev");
+
+    btnz.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        imgPreviewer.classList.remove("hide");
+        imgPreviewer.children[0].src = this.dataset.src;
+        imgPreviewer.children[1].children[0].href = this.dataset.ancher;
+      });
+    });
+  }, 0);
+};
